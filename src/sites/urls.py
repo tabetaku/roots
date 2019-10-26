cfebeb177e5db49d588f57245bbe35a91af285b9
jh.park <jh.park@ridi.com>
2019-10-26 13:11:00 +0900

    Admin URL 변경  - /cms

--- src/sites/urls.py
+++ src/sites/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from apps.domains.hello_world import urls as hello_world_urls
 
 urlpatterns = [
-    path('internal-admin/', admin.site.urls),
+    path('cms/', admin.site.urls),
     path('hello-world/', include(hello_world_urls, namespace='hello_world')),
 ]
 
